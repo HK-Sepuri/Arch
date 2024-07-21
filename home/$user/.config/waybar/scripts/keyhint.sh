9480bc78f21cf511fb2512d69668707b5d56d2b8
HK-Sepuri <harikishore.sepuri@gmail.com>
2024-07-21 08:58:29 +0530

    updated waybar with cheat sheet

---
+++ home/$user/.config/waybar/scripts/keyhint.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+# "Change keyboard layout in" "~/.config/hypr/hyprland.conf" " " \
+
+yad --width=530 --height=620 \
+--center \
+--fixed \
+--title="Arch Hyprland Keybindings" \
+--no-buttons \
+--list \
+--column=Key: \
+--column=Description: \
+--column=Command: \
+--timeout=60 \
+--timeout-indicator=right \
+"ESC" "close this app" "" "=" "modkey" "(mod Mod4)" \
+"+A" "Terminal" "(kitty)" \
+"+R" "Application Menu" "(rofi-wayland)" \
+"+W" "" "(librewolf)" \
+"+E" "" "(thunar)" \
+"+D" "Discord" "(discord)" \
+"+Q" "close focused app" "(kill app)" \
+"+C" "Exit Hyprland" "(kill hyprland)" \
+"+V" "Floating" "(togglefloating)" \
+"+Y" "clipboard manager" "(clipse)" \
+"Print" "screenshot" "(hyprshot output)" \
+"+Print" "screenshot window" "(hyprshot window)" \
+"+Shift+Print" "screenshot region" "(hyprshot region)" \
+"+Left" "Move Focus" "(left)" \
+"+Right" "Move Focus" "(right)" \
+"+Up" "Move Focus" "(up)" \
+"+Down" "Move Focus" "(down)" \
+"+(0-9)" "Move To Workspace" "(change to workspace 0-9)" \
+"+Shift+(0-9)" "Move Active Window To Workspace" "(move app to workspace 0-9)" \
+"+J" "Togglesplit" "Toggles to Vertical/Horizontal" \
+"Fn+F9" "Keyboard Backlight" "(turnoff)" \
+"Fn+F10" "Keyboard Backlight" "(turnon set 50%)" \
+"Fn+F11" "Keyboard Backlight" "(turnon set 100%)" \
+"+MouseLeft" "Move Window" "(drag windows)" \
+"" "" "     Window closed in 60 sec."\
