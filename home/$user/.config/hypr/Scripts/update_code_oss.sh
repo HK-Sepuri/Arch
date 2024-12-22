ac8e8ed70d0e188d1872519f5bc7da80f0e682bc
HK-Sepuri <sepuri.harikishore@gmail.com>
2024-12-22 10:06:31 +0530

    removed qtile

--- home/$user/.config/hypr/Scripts/update_code_oss.sh
+++ home/$user/.config/hypr/Scripts/update_code_oss.sh
@@ -2,6 +2,6 @@
 
 ## Update Code OSS color scheme
 WAL_COLORS_FILE="/home/hari/.cache/wal/colors-vscode.json"
-CODEOSS_SETTINGS_FILE="/home/hari/.config/Code - OSS/User/settings.json"
+CODEOSS_SETTINGS_FILE="/home/hari/.config/Code/User/settings.json"
 # Copy the contents of colours-vscode.json to settings.json
 cp "$WAL_COLORS_FILE" "$CODEOSS_SETTINGS_FILE"
