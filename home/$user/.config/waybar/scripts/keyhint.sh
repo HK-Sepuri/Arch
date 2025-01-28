46de49a1567679fe48476e74a7e1a8ea7e9539cd
LazyButLearning <sepuri.harikishore@gmail.com>
2025-01-28 09:18:18 +0530

    resize windows in hyprland files

--- home/$user/.config/waybar/scripts/keyhint.sh
+++ home/$user/.config/waybar/scripts/keyhint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # "Change keyboard layout in" "~/.config/hypr/hyprland.conf" " " \
 
-yad --width=530 --height=620 \
+yad --width=630 --height=620 \
 --center \
 --fixed \
 --title="Arch Hyprland Keybindings" \
@@ -12,23 +12,22 @@ yad --width=530 --height=620 \
 --column=Command: \
 --timeout=60 \
 --timeout-indicator=right \
-"ESC" "close this app" "" "=" "modkey" "(mod Mod4)" \
+"ESC" "Close this app" "" "=" "Modkey" "(mod Mod4)" \
 "+A" "Terminal" "(kitty)" \
-"+R" "Application Menu" "(rofi-wayland)" \
+"+R" "Application Menu" "(fuzzel)" \
 "+W" "" "(librewolf)" \
-"+E" "" "(thunar)" \
-"+D" "Discord" "(discord)" \
-"+Q" "close focused app" "(kill app)" \
+"+E" "" "(nemo)" \
+"+D" "Discord" "(vesktop)" \
+"+Q" "Close focused app" "(kill app)" \
 "+C" "Exit Hyprland" "(kill hyprland)" \
 "+V" "Floating" "(togglefloating)" \
-"+Y" "clipboard manager" "(clipse)" \
-"Print" "screenshot" "(hyprshot output)" \
-"+Print" "screenshot window" "(hyprshot window)" \
-"Shift+Print" "screenshot region" "(hyprshot region)" \
-"+Left" "Move Focus" "(left)" \
-"+Right" "Move Focus" "(right)" \
-"+Up" "Move Focus" "(up)" \
-"+Down" "Move Focus" "(down)" \
+"+F" "Fullscreen" "(Full Screen)" \
+"+Y" "Clipboard manager" "(clipse)" \
+"Print" "Screenshot" "(hyprshot output)" \
+"+Print" "Screenshot window" "(hyprshot window)" \
+"Shift+Print" "Screenshot region" "(hyprshot region)" \
+"+Arrows" "Move Focus" "(left/right/up/down)" \
+"+Shift+Arrows" "Resize Windows" "(Resize windows left/right/up/down)" \
 "+(0-9)" "Move To Workspace" "(change to workspace 0-9)" \
 "+Shift+(0-9)" "Move Active Window To Workspace" "(move app to workspace 0-9)" \
 "+J" "Togglesplit" "Toggles to Vertical/Horizontal" \
