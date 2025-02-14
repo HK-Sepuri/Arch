637669c4557c2f8651755549b9081230d89619b2
LazyButLearning <sepuri.harikishore@gmail.com>
2025-02-14 07:33:49 +0530

    small changes files

--- home/$user/.config/hypr/Scripts/battery_notify.sh
+++ home/$user/.config/hypr/Scripts/battery_notify.sh
@@ -12,9 +12,9 @@ while true; do
     new_charging_status=$(cat /sys/class/power_supply/ACAD/online)
 
     # Check battery level and send notifications with icons and urgency
-    if [[ "$battery_percent" -ge 80 && "$new_charging_status" -eq 1 ]]; then
+    if [[ "$battery_percent" -ge 83 && "$new_charging_status" -eq 1 ]]; then
         notify-send -i battery-full "Battery Warning" "Battery is at ${battery_percent}%. Please consider stopping charging." -u normal && play_sound
-    elif [[ "$battery_percent" -le 15 && "$new_charging_status" -eq 0 ]]; then
+    elif [[ "$battery_percent" -le 15&& "$new_charging_status" -eq 0 ]]; then
         notify-send -i battery-low "Battery Low" "Battery is low! Current level: ${battery_percent}%. Please charge." -u critical && play_sound
     elif [[ "$battery_percent" -le 5 && "$new_charging_status" -eq 0 ]]; then
         notify-send -i battery-caution "Critical Battery" "Critical battery level! Only ${battery_percent}%. Please charge immediately!" -u critical && play_sound
